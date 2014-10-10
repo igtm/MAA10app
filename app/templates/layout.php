1e43a42adb2ef4afefb7cc8eb67a890f67dcff89
Tomokatsu Iguchi <tomo14916253@gmail.com>
2014-10-10 13:19:27 +0900

    cssOK

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -6,13 +6,13 @@ header("Content-type: text/html; charset=utf-8");?>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="/API/MAA10app/app/style.css">
 <title><?php echo $title;?></title>
 </head>
 <body>
 	<header>
     	<ul class="Nav">
-        	<li class="Nav_item"><a href="./">Homeロゴ</a></li>
+        	<li class="Nav_item"><a href="/API/MAA10app/app">Homeロゴ</a></li>
 			<?php if($isLogin){?>
                 <li class="Nav_item Nav_item-right"><?php echo $_SESSION['name'];?>さん</li>
                 <li class="Nav_item Nav_item-right"><a href="logout">Log out</a></li>
--- tel/inbound_record.php
+++ tel/inbound_record.php
@@ -3,5 +3,8 @@ require '../lib/twilio-php/Services/Twilio.php';
 require '../app/models/model.php';
 
 $voice = $_POST['RecordingUrl'];
+  $updir = "../voices/";
+  $filename = "inoue1.wav";
+move_uploaded_file($voice, $updir.$filename)
 
 ?>
