696bc3a64a572d18309f990c5b48e4ee74e3be96
Tomokatsu Iguchi <tomo14916253@gmail.com>
2014-10-17 15:54:06 +0900

    クラス化中

--- app/models/model.php
+++ app/models/model.php
@@ -1,6 +1,5 @@
 <?php 
 require_once dirname(__FILE__).'/config.php';
-require_once dirname(__FILE__).'/_db.php';
 require_once dirname(__FILE__).'/_auth.php';
 require_once dirname(__FILE__).'/_ModelBase.php';
 require_once dirname(__FILE__).'/_Target.php';
