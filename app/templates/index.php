e19f9e250481832959b3dbd31490c1317dc7371e
Tomokatsu Iguchi <tomo14916253@gmail.com>
2014-10-17 18:56:45 +0900

    クラス化中

--- app/templates/index.php
+++ app/templates/index.php
@@ -1,13 +1,35 @@
-<?php $title = '戦う君へ。ファイトコール！';?>
-<?php $isLogin = false;?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<link rel="stylesheet" type="text/css" href="/API/MAA10app/app/styles/style.css">
+<link rel="stylesheet" type="text/css" href="/API/MAA10app/app/styles/font-awesome.min.css">
+<script type="text/javascript"
+  src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script type="text/javascript"
+  src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
+<script type="text/javascript"
+  src="/API/MAA10app/app/styles/jquery.ui.touch-punch.min.js"></script>
+<script type="text/javascript">
+$(function() {
+});
+</script>
+</head>
 
-<?php ob_start();?>
-<h1><?php echo $title;?></h1>
-<div class="Box">
-このサービスは「みんなの声で大切な人を感動させる」ためのサービスです。<br>
-                皆さんの声をつなぎあわせ、音楽にのせ、大切な人へ送ります。<br>
-                どんな高価な物よりも、みなさんの声が感動させるのです。<br>
-                日頃は、面と向かっていえない感謝の言葉、他愛のないメッセージ、、、など伝えましょう！<br>
-</div>
-<?php $content = ob_get_clean();?>
-<?php include 'layout.php';?>
+<body>
+    <div class="Home">
+    <div class="Home_mask"></div>
+        <div class="Home_box">
+        このサービスは「みんなの声で大切な人を感動させる」ためのサービスです。<br>
+                        皆さんの声をつなぎあわせ、音楽にのせ、大切な人へ送ります。<br>
+                        どんな高価な物よりも、みなさんの声が感動させるのです。<br>
+                        日頃は、面と向かっていえない感謝の言葉、他愛のないメッセージ、、、など伝えましょう！<br>
+        </div>
+        <div class="Home_login">
+            <?php require '_loginBlock.php';?>
+        </div>
+    </div>
+</body>
+</html>
